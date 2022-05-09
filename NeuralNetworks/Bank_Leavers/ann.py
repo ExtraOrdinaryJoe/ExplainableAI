74576a56bfde5570760931866ad85655d87f1093
Priyam05 <35985860+Priyam05@users.noreply.github.com>
2022-05-09 15:19:53 -0400

    Updating the code to use the transformer function

--- NeuralNetworks/Bank_Leavers/ann.py
+++ NeuralNetworks/Bank_Leavers/ann.py
@@ -13,15 +13,10 @@ dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
-# Encoding categorical data
+# Encoding categorical data -- Columns Geograohy and Gender
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
-X = onehotencoder.fit_transform(X).toarray()
-X = X[:, 1:]
+ct = ColumnTransformer([("Transformer", OneHotEncoder(), [1,2])], remainder = 'passthrough')
+X = ct.fit_transform(X)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
